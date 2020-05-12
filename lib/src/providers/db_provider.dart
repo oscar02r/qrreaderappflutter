02fa1fee0b0fd22b57c1b4a36dbfde97b3c599fc
oscar02r <oscary.ramirez@gmail.com>
2020-05-12 04:17:04 +0200

    Adding future deleteAll

--- lib/src/providers/db_provider.dart
+++ lib/src/providers/db_provider.dart
@@ -102,10 +102,17 @@ Future<int> updateScan (ScanModel nuevoScan) async {
 }
 
 Future <int> deleteScan (int id) async {
+  
   final db  = await database;
-
   final res = await db.delete( 'Scans', where: 'id = ?', whereArgs: [id] );
 
+  return res;
+}
+
+Future<int> deleteAll () async {
+
+  final db  = await database;
+  final res = await db.rawDelete('DELETE FROM Scans');
   return res;
 }
 
